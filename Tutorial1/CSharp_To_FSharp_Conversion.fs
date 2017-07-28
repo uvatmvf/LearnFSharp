018987b7bdf0bbfe7669d5f50a80d9100bbe8bbf
Justin McCarthy <uvatmvf@gmail.com>
2017-07-28 13:36:19 -0600

    Add files via upload

---
+++ Tutorial1/CSharp_To_FSharp_Conversion.fs
@@ -0,0 +1,24 @@
+﻿let square x = x * x
+let sumOfSquares n = 
+    [1..n] |> List.map square |> List.sum
+
+sumOfSquares 100
+
+// equivalent c# code for sumOfSquares
+//public static class SumofSquaresHelper
+//{
+//    public static int Square(int i)
+//    { // equivalent of squar efunction
+//        return i * i;
+//    }
+//    public static int SumofSquares(int n)
+//    {
+//        int sum = 0;
+//        for (int i =1; i <= nan; i++)
+//        {
+//            sum += Square(i);
+//        }
+//        return sum;
+//    }
+//}
+
